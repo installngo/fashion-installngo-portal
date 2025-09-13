7d169d97b30507fc96767e397010d4b6093671b8
installngo <installngo.official@gmail.com>
2025-09-13 23:25:23 +0800

    language selector vercel error build fix

--- frontend/src/components/LanguageSelector.tsx
+++ frontend/src/components/LanguageSelector.tsx
@@ -2,11 +2,13 @@
 
 import { useTranslation } from "../context/TranslationContext";
 
+type Language = "english" | "tamil" | "hindi";
+
 export default function LanguageSelector() {
   const { currentLang, setCurrentLang } = useTranslation();
 
   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const selectedLang = e.target.value as "english" | "tamil" | "hindi";
+    const selectedLang = e.target.value as Language;
     setCurrentLang(selectedLang);
   };
 
